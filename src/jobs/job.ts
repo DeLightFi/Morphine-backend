e9d4b4f6d3d67383db69a6ad54a31ff6bcba8d4d
Mathieu M <85166679+Mathaamore@users.noreply.github.com>
2023-01-11 15:48:25 +0100

    add job duration monitoring

--- src/jobs/job.ts
+++ src/jobs/job.ts
@@ -1,20 +1,28 @@
 import * as schedule from "node-schedule";
 import { PoolJob } from "../lib/pool";
 
+let padZero = (v: number, n = 2) => `${v}`.padStart(n, "0");
+let toTime = (v : number) => 
+  `elapsed (hh:mm:ss:ms) ${
+    padZero(Math.floor(v/(60*60000)))}:${
+      padZero(Math.floor(v/60000))}:${
+        padZero(Math.floor(v/1000))}:${
+          padZero(Math.floor(v%1000), 3)}`;
 
 class job {
   constructor() {
 
   }
   public FetchPool() {
-    schedule.scheduleJob('15 * * * *', async function () {
+    schedule.scheduleJob('45 * * * *', async function () {
+      let start = performance.now();
       console.log("Run Pool Fetch")
       const poolfetcher = new PoolJob(
         "morphine-indexer-1",
         "goerli-2.starknet.stream.apibara.com:443"
       );
       await poolfetcher.run()
-      console.log("--- end")
+      console.log(`--- end | ${toTime(performance.now() - start)}`)
     });
   }
 }
